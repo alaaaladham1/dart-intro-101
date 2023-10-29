c4f0eaca3b402173de275987402d052e58fd6bfb
alaaaladham1 <149304004+alaaaladham1@users.noreply.github.com>
2023-10-29 15:43:10 +0300

    we created a new dart project

---
+++ dart_into_sdkcenter/bin/dart_into_sdkcenter.dart
@@ -0,0 +1,5 @@
+import 'package:dart_into_sdkcenter/dart_into_sdkcenter.dart' as dart_into_sdkcenter;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_into_sdkcenter.calculate()}!');
+}
---
+++ dart_into_sdkcenter/test/dart_into_sdkcenter_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_into_sdkcenter/dart_into_sdkcenter.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
